5f9520cae64cbcfb17259533e149a604c2f7e68a
Witek Socha <witek.soch@gmail.com>
2026-01-17 16:05:55 +0100

    Show pollen under bee when carrying a flower

--- src/game/objects/Bee.ts
+++ src/game/objects/Bee.ts
@@ -4,10 +4,14 @@ import { ANIM_KEYS, ASSET_KEYS } from '../constants';
 export class Bee
 {
     private readonly sprite: Phaser.GameObjects.Sprite;
+    private readonly pollenImages: Phaser.GameObjects.Image[] = [];
     private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
     private readonly flySpeed = 220;
     private readonly turnSpeed = 2.5;
     private readonly scale = 0.15;
+    private readonly pollenScale = 0.18;
+    private readonly pollenOffsetX = 10;
+    private readonly pollenOffsetY = 22;
     private carriedFlowerKey?: string;
 
     constructor (scene: Scene, x: number, y: number)
@@ -16,6 +20,7 @@ export class Bee
 
         this.sprite = scene.add.sprite(x, y, ASSET_KEYS.Bee1);
         this.sprite.setOrigin(0.5).setScale(this.scale).play(ANIM_KEYS.BeeFly);
+        this.createPollen(scene);
     }
 
     setControls (cursors: Phaser.Types.Input.Keyboard.CursorKeys | undefined): void
@@ -27,6 +32,7 @@ export class Bee
     {
         this.handleInput(dt);
         this.wrap(wrapRect);
+        this.updatePollen();
     }
 
     hasFlower (): boolean
@@ -42,6 +48,7 @@ export class Bee
         }
 
         this.carriedFlowerKey = flowerKey;
+        this.setPollenVisible(true);
         return true;
     }
 
@@ -49,12 +56,50 @@ export class Bee
     {
         const key = this.carriedFlowerKey;
         this.carriedFlowerKey = undefined;
+        this.setPollenVisible(false);
         return key;
     }
 
     getBounds (output?: Phaser.Geom.Rectangle): Phaser.Geom.Rectangle
     {
         return this.sprite.getBounds(output);
+    }
+
+    private createPollen (scene: Scene): void
+    {
+        const left = scene.add.image(this.sprite.x, this.sprite.y, ASSET_KEYS.Pollen).setScale(this.pollenScale);
+        const right = scene.add.image(this.sprite.x, this.sprite.y, ASSET_KEYS.Pollen).setScale(this.pollenScale);
+        this.pollenImages.push(left, right);
+        this.setPollenVisible(false);
+    }
+
+    private setPollenVisible (visible: boolean): void
+    {
+        for (const pollen of this.pollenImages)
+        {
+            pollen.setVisible(visible);
+        }
+    }
+
+    private updatePollen (): void
+    {
+        if (!this.carriedFlowerKey || this.pollenImages.length === 0)
+        {
+            return;
+        }
+
+        const offsets = [
+            new Phaser.Math.Vector2(-this.pollenOffsetX, this.pollenOffsetY),
+            new Phaser.Math.Vector2(this.pollenOffsetX, this.pollenOffsetY)
+        ];
+
+        for (let i = 0; i < this.pollenImages.length; i += 1)
+        {
+            const rotated = offsets[i].rotate(this.sprite.rotation);
+            const pollen = this.pollenImages[i];
+            pollen.setPosition(this.sprite.x + rotated.x, this.sprite.y + rotated.y);
+            pollen.setRotation(this.sprite.rotation);
+        }
     }
 
     private ensureAnimation (scene: Scene): void
